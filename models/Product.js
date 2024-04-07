47a689103c58fcf00ec61941de0bb6ad2db004b4
Jherwin San Juan <sanju.jherwin@gmail.com>
2024-04-07 17:51:31 +0800

    added changes

---
+++ models/Product.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is Required"],
+  },
+  image: {
+    type: String,
+    required: [true, "Image file is Required"],
+  },
+  category: {
+    type: String,
+    required: [true, "Category is Required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is Required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is Required"],
+  },
+  inventoryStock: {
+    type: Number,
+    required: [true, "Inventory stock is Required"],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
